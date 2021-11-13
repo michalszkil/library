ca95ea6bdd468d8aba828b4877cd085d5029df42
Michal Szkil <michalszkil@gmail.com>
2021-11-14 00:44:47 +0100

    Added basic input functionality

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -9,5 +11,13 @@ function Book(title, author, pages, read) {
     }
 }
 
-theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-console.log(theHobbit.info());
+function addBookToLibrary() {
+    let title = prompt("What's the title of the book?");
+    console.log(title);
+    let author = prompt("Who's the author?");
+    let pages = prompt("How many pages?");
+    let read = prompt("Have you read it already?");
+}
+// addBookToLibrary();
+// theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+// console.log(theHobbit.info());
